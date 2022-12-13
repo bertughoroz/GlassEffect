7589a5553628415f3463e77428a703c1fdc48cf2
Bertug <ber2horoz@gmail.com>
2022-12-13 17:31:30 +0300

    Initial Commit

---
+++ GlassEffect/GlassEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassEffectApp.swift
+//  GlassEffect
+//
+//  Created by Bertuğ Horoz on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GlassEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
